53030b3f1cd5f682e80723fa9de6b9d4412c9ab4
Tyler Sargent <tsargent@gmail.com>
2020-03-29 14:35:01 -0400

    gets state data

--- plugins/gatsby-source-nytimes-covid-19/gatsby-node.js
+++ plugins/gatsby-source-nytimes-covid-19/gatsby-node.js
@@ -1,13 +1,32 @@
 const request = require("request")
-exports.sourceNodes = () => {
+const csv = require("csvtojson")
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
 
-  const csvCounties = `https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv`
-  request.get(csvCounties, (error, response, body) => {
-    console.log('csvCounties', body);
-  });
+  const { createNode } = actions
 
-  const csvStates = `https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv`
-  request.get(csvStates, (error, response, body) => {
-    console.log('body', body);
-  });
+  const processRow = row => {
+    const nodeId = createNodeId(`nytimes-covid-19-state-${row.fips}-${row.date}`)
+    const nodeContent = JSON.stringify(row)
+    const nodeData = {
+      ...row,
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: `NYTimesCovid19State`,
+        content: nodeContent,
+        contentDigest: createContentDigest(row),
+      }
+    }
+    return nodeData;
+  }
+
+  const statesUrl = `https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv`
+
+  return csv().fromStream(request.get(statesUrl)).subscribe(json => {
+    return new Promise((resolve) => {
+      const nodeData = processRow(json);
+      resolve(createNode(nodeData));
+    })
+  })
 }
