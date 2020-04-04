0fd17df8b115fe71cdf8d34ba8c976e0cfabd218
Tyler Sargent <tsargent@gmail.com>
2020-04-04 14:30:02 -0400

    add counties in source

--- plugins/gatsby-source-nytimes-covid-19/gatsby-node.js
+++ plugins/gatsby-source-nytimes-covid-19/gatsby-node.js
@@ -1,10 +1,11 @@
 const request = require("request")
 const csv = require("csvtojson")
-exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
-
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
   const { createNode } = actions
+  const statesUrl = `https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv`
+  const countiesUrl = `https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv` 
 
-  const processRow = row => {
+  const processStateRow = row => {
     const nodeId = createNodeId(`nytimes-covid-19-state-${row.fips}-${row.date}`)
     const nodeContent = JSON.stringify(row)
     const nodeData = {
@@ -21,12 +22,33 @@ exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
     return nodeData;
   }
 
-  const statesUrl = `https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv`
+  const processCountyRow = row => {
+    const nodeId = createNodeId(`nytimes-covid-19-county-${row.fips}-${row.date}`)
+    const nodeContent = JSON.stringify(row)
+    const nodeData = {
+      ...row,
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: `NYTimesCovid19County`,
+        content: nodeContent,
+        contentDigest: createContentDigest(row),
+      }
+    }
+    return nodeData;
+  }
 
-  return csv().fromStream(request.get(statesUrl)).subscribe(json => {
-    return new Promise((resolve) => {
-      const nodeData = processRow(json);
-      resolve(createNode(nodeData));
-    })
-  })
+  const statesData = await csv().fromStream(request.get(statesUrl));
+  const countiesData = await csv().fromStream(request.get(countiesUrl));
+
+  statesData.forEach(state => {
+    const nodeData = processStateRow(state);
+    createNode(nodeData);
+  });
+
+  countiesData.forEach(county => {
+    const nodeData = processCountyRow(county);
+    createNode(nodeData);
+  });
 }
