6f915bd41738bbac32ba85f6f18d3318961773e8
Tyler Sargent <tsargent@gmail.com>
2020-04-04 12:48:03 -0400

    remove styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,29 +3,14 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <h1 style={{ margin: 0 }}>
+      <Link
+        to="/"
+      >
+        {siteTitle}
+      </Link>
+    </h1>
   </header>
 )
 
